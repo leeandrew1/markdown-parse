d90a89581fe527106993204f4e5add8b35829f2e
leeandrew1 <andrewlee3250@gmail.com>
2022-01-27 13:51:39 -0800

    added test4 and changes
    we made to make sure the nextOpenBracket is at the position of openParen - 1

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,7 +16,9 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if (nextOpenBracket == 0 || markdown.charAt(nextOpenBracket - 1) != '!') {
+
+            if (nextCloseBracket == openParen - 1 && (nextCloseBracket == 0
+                    || markdown.charAt(nextOpenBracket - 1) != '!')) {
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
             currentIndex = closeParen + 1;
