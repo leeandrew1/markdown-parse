34bbfdb9878c56937e129be0b526b917314f8d12
leea1 <asl011@ucsd.edu>
2022-02-24 12:59:59 -0800

    .

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -5,54 +5,54 @@
 // import java.nio.file.Path;
 // import java.util.ArrayList;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
+// import java.io.IOException;
+// import java.nio.file.Files;
+// import java.nio.file.Path;
+// import java.util.ArrayList;
 
-// comment z
-public class MarkdownParse {
-    public static ArrayList<String> getLinks(String markdown) {
-        // random comment
-        ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then take up to
-        // the next )
-        int currentIndex = 0;
-        while (currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            System.out.println(currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            // the second argument is for where to start research, if not provided, search
-            // start from index 0
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
+// // comment z
+// public class MarkdownParse {
+//     public static ArrayList<String> getLinks(String markdown) {
+//         // random comment
+//         ArrayList<String> toReturn = new ArrayList<>();
+//         // find the next [, then find the ], then find the (, then take up to
+//         // the next )
+//         int currentIndex = 0;
+//         while (currentIndex < markdown.length()) {
+//             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+//             System.out.println(currentIndex);
+//             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+//             // the second argument is for where to start research, if not provided, search
+//             // start from index 0
+//             int openParen = markdown.indexOf("(", nextCloseBracket);
+//             int closeParen = markdown.indexOf(")", openParen);
 
-            if (nextCloseBracket + 1 == openParen
-                    && (nextOpenBracket == 0 || markdown.charAt(nextOpenBracket - 1) != '!')) {
+//             if (nextCloseBracket + 1 == openParen
+//                     && (nextOpenBracket == 0 || markdown.charAt(nextOpenBracket - 1) != '!')) {
 
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-                int whiteSpace = markdown.indexOf(" ", openParen);
-                for (int i = openParen; i < closeParen; ++i) {
-                    if (whiteSpace == i) {
-                        toReturn.remove(markdown.substring(openParen + 1, closeParen));
+//                 toReturn.add(markdown.substring(openParen + 1, closeParen));
+//                 int whiteSpace = markdown.indexOf(" ", openParen);
+//                 for (int i = openParen; i < closeParen; ++i) {
+//                     if (whiteSpace == i) {
+//                         toReturn.remove(markdown.substring(openParen + 1, closeParen));
 
-                    }
-                }
-                // github push test
-            }
-            currentIndex = closeParen + 1;
-            System.out.println(currentIndex);
-        }
-        return toReturn;
-    }
+//                     }
+//                 }
+//                 // github push test
+//             }
+//             currentIndex = closeParen + 1;
+//             System.out.println(currentIndex);
+//         }
+//         return toReturn;
+//     }
 
-    public static void main(String[] args) throws IOException {
-        Path fileName = Path.of(args[0]);
-        String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
-        System.out.println(links);
-    }
-}
+//     public static void main(String[] args) throws IOException {
+//         Path fileName = Path.of(args[0]);
+//         String contents = Files.readString(fileName);
+//         ArrayList<String> links = getLinks(contents);
+//         System.out.println(links);
+//     }
+// }
 
 // //
 // public class MarkdownParse {
@@ -87,3 +87,45 @@ public class MarkdownParse {
 // System.out.println(links);
 // }
 // }
+
+// 1pm markdown parse
+// File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class MarkdownParse {
+    public static ArrayList<String> getLinks(String markdown) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        // find the next [, then find the ], then find the (, then take up to
+        // the next )
+        int currentIndex = 0;
+        while (currentIndex < markdown.length()) {
+            int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            if (nextOpenBracket == -1) {
+                break;
+            }
+            if (nextOpenBracket > 0 && markdown.charAt(nextOpenBracket - 1) == '!') {
+                break;
+            }
+            int nextCloseBracket = markdown.indexOf("](", nextOpenBracket);
+            if (nextCloseBracket == -1) {
+                break;
+            }
+            int closeParen = markdown.indexOf(")", nextCloseBracket);
+            toReturn.add(markdown.substring(nextCloseBracket + 2, closeParen));
+            currentIndex = closeParen + 1;
+            // System.out.format("%d\t%d\t%d\t%d\n", currentIndex, nextOpenBracket,
+            // nextCloseBracket, closeParen);
+        }
+        return toReturn;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Path fileName = Path.of(args[0]);
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = getLinks(contents);
+        System.out.println(links);
+    }
+}
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,75 +1,136 @@
+// import static org.junit.Assert.*;
+// import org.junit.*;
+// import java.io.IOException;
+// import java.nio.file.Files;
+// import java.nio.file.Path;
+// import java.util.List;
+
+// public class MarkdownParseTest {
+//     @Test
+//     public void addition() {
+//         assertEquals(2, 1 + 1);
+//     }
+
+//     @Test
+//     public void getLinks() throws IOException {
+//         // Path fileName =
+//         // Path.of("C:/Users/cowhe/Documents/GitHub/markdown-parse/test-file.md");
+//         Path fileName = Path.of("test-file.md");
+//         String contents = Files.readString(fileName);
+//         assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
+//     }
+
+//     @Test
+//     public void getLinks1() throws IOException {
+//         // Path fileName =
+//         // Path.of("C:/Users/cowhe/Documents/GitHub/markdown-parse/test2.md");
+//         Path fileName = Path.of("test2.md");
+//         String contents = Files.readString(fileName);
+//         assertEquals(List.of("link.com", ""), MarkdownParse.getLinks(contents));
+//     }
+
+//     @Test
+//     public void getLinks2() throws IOException {
+//         // Path fileName =
+//         // Path.of("C:/Users/cowhe/Documents/GitHub/markdown-parse/test3.md");
+//         Path fileName = Path.of("test3.md");
+//         String contents = Files.readString(fileName);
+//         assertEquals(List.of(""), MarkdownParse.getLinks(contents));
+//     }
+
+//     @Test
+//     public void getLinks3() throws IOException {
+//         // Path fileName =
+//         // Path.of("C:/Users/cowhe/Documents/GitHub/markdown-parse/test4.md");
+//         Path fileName = Path.of("test4.md");
+//         String contents = Files.readString(fileName);
+//         assertEquals(List.of(), MarkdownParse.getLinks(contents));
+//     }
+
+// }
+
+// // public class MarkdownParseTest {
+// // @Test
+// // public void addition() {
+// // assertEquals(2, 1 + 1);
+// // }
+
+// // @Test
+// // public void test() throws IOException {
+// // Path fileName = Path.of("test-file.md");
+// // String contents = Files.readString(fileName);
+// // assertEquals(MarkdownParse.getLinks(contents),
+// // List.of("https://something.com", "some-page.html"));
+
+// // Path fileName2 = Path.of("test2.md");
+// // String contents2 = Files.readString(fileName2);
+// // assertEquals(MarkdownParse.getLinks(contents2), List.of("link.com"));
+// // }
+// // }
+
+// // javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar
+// // MarkdownParseTest.java
+
+// // java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar
+// // org.junit.runner.JUnitCore MarkdownParseTest
+
 import static org.junit.Assert.*;
 import org.junit.*;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
 public class MarkdownParseTest {
+
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+    public void testFile1() throws IOException {
+        String contents = Files.readString(Path.of("./test-file.md"));
+        List<String> expect = List.of("https://something.com", "some-page.html");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
     }
 
     @Test
-    public void getLinks() throws IOException {
-        // Path fileName =
-        // Path.of("C:/Users/cowhe/Documents/GitHub/markdown-parse/test-file.md");
-        Path fileName = Path.of("test-file.md");
-        String contents = Files.readString(fileName);
-        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
+    public void testFile2() throws IOException {
+        String contents = Files.readString(Path.of("./test-file2.md"));
+        List<String> expect = List.of("https://something.com", "some-page.html");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
     }
 
     @Test
-    public void getLinks1() throws IOException {
-        // Path fileName =
-        // Path.of("C:/Users/cowhe/Documents/GitHub/markdown-parse/test2.md");
-        Path fileName = Path.of("test2.md");
-        String contents = Files.readString(fileName);
-        assertEquals(List.of("link.com", ""), MarkdownParse.getLinks(contents));
+    public void testSingleImage() throws IOException {
+        String contents = Files.readString(Path.of("./test-single-image.md"));
+        List<String> expect = List.of();
+        assertEquals(MarkdownParse.getLinks(contents), expect);
     }
 
     @Test
-    public void getLinks2() throws IOException {
-        // Path fileName =
-        // Path.of("C:/Users/cowhe/Documents/GitHub/markdown-parse/test3.md");
-        Path fileName = Path.of("test3.md");
-        String contents = Files.readString(fileName);
-        assertEquals(List.of(""), MarkdownParse.getLinks(contents));
+    public void testLinkAtBeginning() {
+        String contents = "[link title](a.com)";
+        List<String> expect = List.of("a.com");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
     }
 
     @Test
-    public void getLinks3() throws IOException {
-        // Path fileName =
-        // Path.of("C:/Users/cowhe/Documents/GitHub/markdown-parse/test4.md");
-        Path fileName = Path.of("test4.md");
-        String contents = Files.readString(fileName);
-        assertEquals(List.of(), MarkdownParse.getLinks(contents));
+    public void testSpaceInURL() {
+        String contents = "[title](space in-url.com)";
+        List<String> expect = List.of();
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
+    @Test
+    public void testSpaceAfterParen() {
+        String contents = "[title]( space-in-url.com)";
+        List<String> expect = List.of("space-in-url.com");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testSpaceBeforeParen() {
+        String contents = "[title]   (should-not-count.com)";
+        List<String> expect = List.of();
+        assertEquals(MarkdownParse.getLinks(contents), expect);
     }
 
 }
-
-// public class MarkdownParseTest {
-// @Test
-// public void addition() {
-// assertEquals(2, 1 + 1);
-// }
-
-// @Test
-// public void test() throws IOException {
-// Path fileName = Path.of("test-file.md");
-// String contents = Files.readString(fileName);
-// assertEquals(MarkdownParse.getLinks(contents),
-// List.of("https://something.com", "some-page.html"));
-
-// Path fileName2 = Path.of("test2.md");
-// String contents2 = Files.readString(fileName2);
-// assertEquals(MarkdownParse.getLinks(contents2), List.of("link.com"));
-// }
-// }
-
-// javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar
-// MarkdownParseTest.java
-
-// java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar
-// org.junit.runner.JUnitCore MarkdownParseTest
