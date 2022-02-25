d4977134d03365a0243945074a971c2fbb0f4a03
andrew <andrewlee3250@gmail.com>
2022-02-24 16:47:20 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 // // comment z
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
-        // random comment
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
         // the next )
@@ -27,18 +26,9 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
 
-            if (nextCloseBracket + 1 == openParen
+            if (nextCloseBracket + 1 == openParen && closeParen - openParen != 1
                     && (nextOpenBracket == 0 || markdown.charAt(nextOpenBracket - 1) != '!')) {
-
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
-                int whiteSpace = markdown.indexOf(" ", openParen);
-                for (int i = openParen; i < closeParen; ++i) {
-                    if (whiteSpace == i) {
-                        toReturn.remove(markdown.substring(openParen + 1, closeParen));
-
-                    }
-                }
-                // github push test
             }
             currentIndex = closeParen + 1;
             System.out.println(currentIndex);
